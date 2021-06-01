ba31926c6edd7cef810f091d8988eaa1077a8dc2
Matt Harris <711mattman@gmail.com>
2021-06-01 12:16:53 -0400

    Deleted the intial source folder and added my own. Won't be needing the additional files:

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import react from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import app from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
