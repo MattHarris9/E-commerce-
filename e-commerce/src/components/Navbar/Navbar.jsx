537bf0f60792289b83a8170a6af5394feb79daf3
Matt Harris <711mattman@gmail.com>
2021-07-02 11:42:28 -0400

    Added logic into the nav bar that only shows the number of items in the cart only on the home page.

--- e-commerce/src/components/Navbar/Navbar.jsx
+++ e-commerce/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import { LocationOff, ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from "../../assets/commerce.jpg";
 import useStyles from "./styles";
 
+
+
 const Navbar = ({ totalItems  }) => {
     const classes = useStyles();
+    const location = useLocation();
 
     return (
         
@@ -19,13 +22,14 @@ const Navbar = ({ totalItems  }) => {
                         Commerce.js
                     </Typography>
                     <div className={classes.grow} />
+                    {location.pathname === '/' && (
                     <div className={classes.button}>
                         <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary" >
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div> )}
                 </Toolbar>
 
             </AppBar>
