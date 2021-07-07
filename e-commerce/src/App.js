422e6bb69641fba9a728cf4b3fb63a46298cd511
Matt Harris <711mattman@gmail.com>
2021-07-07 11:37:40 -0400

    Got the add, remove, delete functions working for the cart

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -14,15 +14,34 @@ const App = () => {
         setProducts(data);
     };
 
+
     const fetchCart = async () => {
         setCart(await commerce.cart.retrieve()); 
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
+        const { cart } = await commerce.cart.add(productId, quantity);
 
-        setCart(item.cart); 
+        setCart(cart); 
     };
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity })
+
+        setCart(cart);
+    };
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+
+        setCart(cart);
+    };
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+
+        setCart(cart);
+    }
 
     useEffect(() => {
         fetchProducts();
@@ -33,16 +52,21 @@ const App = () => {
 
     return (
     <Router>
-        <div>
+        <div>``
             <Navbar totalItems={cart.total_items} />
             <Switch>
                 <Route exact path="/">
-                    <Products products= {products} onAddToCart = {handleAddToCart} />  
+                    <Products products={products} onAddToCart={handleAddToCart} />  
 
                 </Route>
 
                 <Route exact path="/cart">
-                    <Cart cart={cart} />
+                    <Cart 
+                    cart={cart} 
+                    handleUpdateCartQty={handleUpdateCartQty}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                    handleEmptyCart={handleEmptyCart}
+                    />
                 </Route>
               
             </Switch>
--- e-commerce/src/components/Cart/CartItem/CartItem.jsx
+++ e-commerce/src/components/Cart/CartItem/CartItem.jsx
@@ -3,8 +3,8 @@ import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@
 
 import useStyles from "./styles";
 
-const CartItem = ({ item }) => {
-    const classes = useStyles();
+const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
+    const classes = useStyles();;
     
     
     return (
@@ -16,11 +16,11 @@ const CartItem = ({ item }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <div className={classes.buttons}>
-                    <Button type="button" size="small">-</Button>
+                    <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small">+</Button>
+                    <Button type="button" size="small" onClick={() => onUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
                 </div>
-                <Button varient="conatained" type="button" color="secondary">Remove</Button>
+                <Button varient="conatained" type="button" color="secondary" onClick={() => onRemoveFromCart(item.id)}>Remove</Button>
             </CardActions>
             
         </Card>
