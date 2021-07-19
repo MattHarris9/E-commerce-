aa21ac5bbfe4dc3ad4de7ca741670cae80c99422
Matt Harris <711mattman@gmail.com>
2021-07-19 10:55:44 -0400

    Added the text fields for the address form

--- e-commerce/src/components/CheckoutForm/AddressForm.jsx
+++ e-commerce/src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { InputLable, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import FormInput from "./CustomTextField";
 
 const AddressForm = () => {
+    const methods = useForm();
+    
+
     return (
-        <div>
-            Address Form
-        </div>
+        <React.Fragment>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name="firstname" label="First Name" />
+                        <FormInput required name="lastname" label="Last Name" />
+                        <FormInput required name="address1" label="Address" />
+                        <FormInput required name="email" label="Email" />
+                        <FormInput required name="city" label="City" />
+                        <FormInput required name="zip" label="Zip / Postal code" />
+                    </Grid>
+                </form>
+            </FormProvider>
+        </React.Fragment>
     )
 }
 
